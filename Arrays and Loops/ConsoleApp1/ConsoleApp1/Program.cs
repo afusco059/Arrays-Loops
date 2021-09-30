d6baaf1ead01b142938250d2e5778872401e124e
afusco059 <70164839+afusco059@users.noreply.github.com>
2021-09-30 10:03:55 -0400

    Add files via upload

---
+++ Arrays and Loops/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] goodBands;
+
+            goodBands = new string[] { "Papa Roach", "Ice Nine kills", "Memphis May Fire", "Metallica", "Avatar", "AC/DC", "Slayer", "D12" };
+
+
+
+            string[] goodBandsCopy = new string[8];
+
+            Array.Copy(goodBands, goodBands, 8);
+            Console.WriteLine(goodBands[0]);
+            Console.WriteLine(goodBands[1]);
+            Console.WriteLine(goodBands[2]);
+            Console.WriteLine(goodBands[3]);
+            Console.WriteLine(goodBands[4]);
+            Console.WriteLine(goodBands[5]);
+            Console.WriteLine(goodBands[6]);
+            Console.WriteLine(goodBands[7]);
+
+            Array.Reverse(goodBands);
+            Console.WriteLine(goodBands[0]);
+            Console.WriteLine(goodBands[1]);
+            Console.WriteLine(goodBands[2]);
+            Console.WriteLine(goodBands[3]);
+            Console.WriteLine(goodBands[4]);
+            Console.WriteLine(goodBands[5]);
+            Console.WriteLine(goodBands[6]);
+            Console.WriteLine(goodBands[7]);
+
+            Console.ReadKey();
+        }
+        static void Loops(string[] args)
+        {
+            bool buttonClick = false;
+            int counter = 10;
+
+            do
+            {
+                Console.WriteLine("Woah Not again");
+                counter++;
+
+                if (counter == 15)
+                {
+                    Console.WriteLine("Finally It's over");
+                    break;
+                }
+            } while (!buttonClick);
+        }
+    }
+}
